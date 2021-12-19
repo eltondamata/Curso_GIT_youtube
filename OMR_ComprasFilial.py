3c99f09bc3331bbf4abc5adf180508ef03096799
eltondamata <eltondamata@gmail.com>
2021-12-19 17:21:02 -0300

    formatacao do codigo.py

--- OMR_ComprasFilial.py
+++ OMR_ComprasFilial.py
@@ -1,39 +1,12 @@
-#!/usr/bin/env python
+#eltondamata@gmail.com
 # coding: utf-8
-
-# In[1]:
 
 
 import pandas as pd
 
-
-# In[2]:
-
-
 df = pd.read_pickle('~/Downloads/OMR_COMPRASFILIAL.pkl')
-
-
-# In[3]:
-
-
 display(df)
-
-
-# In[11]:
-
 
 df2 = df.groupby('DESTIPCNLVNDOMR')[['VLRVNDFATLIQ']].sum()
 display(df2)
-
-
-# In[8]:
-
-
-display(df2)
-
-
-# In[ ]:
-
-
-
-
+display(df2)s
